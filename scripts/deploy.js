63992dc5043b00c4f6bc60b7dba19a131d1b6a77
drlappies <yau1chun2lap3@gmail.com>
2022-03-19 20:52:13 +0800

    add: deploy

---
+++ scripts/deploy.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+
+const HDWalletProvider = require("@truffle/hdwallet-provider")
+const Web3 = require("web3");
+const { abi, evm } = require("./compile");
+
+
+const provider = new HDWalletProvider(process.env.PHRASE, process.env.INFURA_ENDPOINT)
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    try {
+        const accounts = await web3.eth.getAccounts();
+        console.log("attemping to deploy from account: ", accounts[0])
+
+        const contract = await new web3.eth.Contract(abi).deploy({
+            data: evm.bytecode.object,
+        }).send({ gas: '1000000', from: accounts[0] });
+
+        console.log('Contract deployed to', contract.options.address);
+        provider.engine.stop()
+    } catch (error) {
+        console.log(error)
+        provider.engine.stop()
+    }
+}
+
+deploy();
